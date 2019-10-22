4745e3cf615660808bd5aec86335fa7c7b6bd4df
petecarapetyan <pete@datafundamentals.com>
2019-10-22 10:54:38 -0500

    running the app itself at least brings up opening pages

--- NOTES.md
+++ NOTES.md
@@ -2,3 +2,35 @@
 
 https://graphql.appwriter.com 
 
+## Next Steps
+
+- src/...view, src/...delete etc pages need to be fleshed out with at least a title
+- navigation from each of the views to the next 
+- first actual functionality against graphql
+
+## Sequence to Replicate
+
+At least, to this point
+
+  - opened up https://aws-amplify.github.io/docs/js/start?platform=purejs
+  - mkdir graphql.appwriter
+  - npm init @open-wc took all defaults and package name of graphql.appwriter
+  - added two dependencies to package.json per above aws-amplify docs
+      "@aws-amplify/api": "latest",
+      "@aws-amplify/pubsub": "latest"
+  - yarn
+  - had to re-install amplify go figure npm install -g @aws-amplify/cli
+  - amplify init
+  - amplify add api
+  - amplify push
+      used javascript as the code generation language target, not typescript
+  - copied
+      index.ts from zospeum for the preset code
+      your-plugin.js from rPlugin for the plugin code
+      codegen.yml from rPlugin
+  - repathed and renamed above 3
+  - copied 4 graphql dev dependencies from rPlugin
+  - copied start script from rPlugin and renamed gen
+  - [magic happened here]
+  - it runs with `npm run gen`
+  - launch the app itself with `npm start`
--- src/my-plugin.js
+++ src/my-plugin.js
@@ -1,36 +1,37 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { printSchema, parse, visit } = require('graphql');
 
-const parseType = (rawType) => {
+const parseType = rawType => {
   if (rawType.kind === 'NamedType') {
     return {
       type: rawType.name.value,
       isNullable: true,
-      isList: false
+      isList: false,
     };
   }
   if (rawType.kind === 'NonNullType') {
     return {
       ...parseType(rawType.type),
       isNullable: false,
-      isList: false
+      isList: false,
     };
   }
   if (rawType.kind === 'ListType') {
     return {
       ...parseType(rawType.type),
       isNullable: true,
-      isList: true
+      isList: true,
     };
   }
   return { type: rawType.kind, isNullable: false };
-}
+};
 
 const getModelUpsert = (name, fields) => {
   const blah = fields.map(field => `blah ${field.name} foobar ${field.type}`);
   return `import { html, css, LitElement } from 'lit-element';
+  window.customElements.define('${name.toLowerCase()}-view', ${name}View);
 
-  export class PageModels extends LitElement {
+  export class ${name}Upsert) extends LitElement {
     static get styles() {
       return css\`
         :host {
@@ -60,20 +61,20 @@ const getModelUpsert = (name, fields) => {
     render() {
       return html\`
         <h2>$\{this.title} Nr. $\{this.counter}!</h2>
-${blah.join("\n")}
+${blah.join('\n')}
         <button @click=$\{this.__increment}>increment</button>
       \`;
     }
   }
+  window.customElements.define('${name.toLowerCase()}-upsert', ${name}Upsert);
   
   `;
-}
+};
 
 const getModelView = (name, fields) => {
   const blah = fields.map(field => `blah ${field.name} foobar ${field.type}`);
   return `import { html, css, LitElement } from 'lit-element';
-
-  export class PageModels extends LitElement {
+  export class ${name}View extends LitElement {
     static get styles() {
       return css\`
         :host {
@@ -103,32 +104,29 @@ const getModelView = (name, fields) => {
     render() {
       return html\`
         <h2>$\{this.title} Nr. $\{this.counter}!</h2>
-${blah.join("\n")}
+${blah.join('\n')}
         <button @click=$\{this.__increment}>increment</button>
       \`;
     }
   }
   
+  window.customElements.define('${name.toLowerCase()}-view', ${name}View);
   `;
-}
+};
 
-
-const getModelsIndex = (name) => {
-  return `export { PageModels } from './src/PageModels.js';
+const getModelsIndex = () => `export { PageModels } from './src/PageModels.js';
   `;
-}
 
-
-const getIndexModel = (name) => {
-  return `export { ${name}Model } from './src/${name}Model.js';
+const getIndexModel = name => `export { ${name}Model } from './src/${name}Model.js';
   `;
-}
 
 const getPageModels = (name, nodes) => {
   const links = nodes.map(node => `\t\t\t\t<p> ${node}</p>`);
-  const imports = nodes.map(node => `import '../../model-${node.toLowerCase()}/model-${node.toLowerCase()}.js';`);
+  const imports = nodes.map(
+    node => `import '../../model-${node.toLowerCase()}/model-${node.toLowerCase()}.js';`,
+  );
   return `import { html, css, LitElement } from 'lit-element';
-${imports.join("\n")}
+${imports.join('\n')}
 
   export class PageModels extends LitElement {
     static get styles() {
@@ -160,27 +158,25 @@ ${imports.join("\n")}
     render() {
       return html\`
         <h2>$\{this.title} Nr. $\{this.counter}!</h2>
-${links.join("\n")}
+${links.join('\n')}
         <button @click=$\{this.__increment}>increment</button>
       \`;
     }
   }
   
   `;
-}
+};
 
-const getModelModel = (name) => {
-  return `import { ${name}Model } from './src/${name}Model.js';
+const getModelModel = name => `import { ${name}Model } from './src/${name}Model.js';
 
   window.customElements.define('model-${name.toLowerCase()}', ${name}Model);
   `;
-}
 
 const getModelSource = (name, fields) => {
   const blah = fields.map(field => `blah ${field.name} foobar ${field.type}`);
   return `import { html, css, LitElement } from 'lit-element';
 
-  export class PageModels extends LitElement {
+  export class ${name}Model extends LitElement {
     static get styles() {
       return css\`
         :host {
@@ -210,27 +206,25 @@ const getModelSource = (name, fields) => {
     render() {
       return html\`
         <h2>$\{this.title} Nr. $\{this.counter}!</h2>
-${blah.join("\n")}
+${blah.join('\n')}
         <button @click=$\{this.__increment}>increment</button>
       \`;
     }
   }
   
   `;
-}
+};
 
-const getModelsSource = (name) => {
-  return `import { PageModels } from './src/PageModels.js';
+const getModelsSource = () => `import { PageModels } from './src/PageModels.js';
 
   window.customElements.define('page-models', PageModels);
   `;
-}
 
 const getModelDelete = (name, fields) => {
   const blah = fields.map(field => `blah ${field.name} foobar ${field.type}`);
   return `import { html, css, LitElement } from 'lit-element';
 
-  export class PageModels extends LitElement {
+  export class ${name}Delete extends LitElement {
     static get styles() {
       return css\`
         :host {
@@ -260,63 +254,63 @@ const getModelDelete = (name, fields) => {
     render() {
       return html\`
         <h2>$\{this.title} Nr. $\{this.counter}!</h2>
-${blah.join("\n")}
+${blah.join('\n')}
         <button @click=$\{this.__increment}>increment</button>
       \`;
     }
   }
   
+  window.customElements.define('${name.toLowerCase()}-delete', ${name}Delete);
   `;
-}
+};
 
 const extractFields = (name, types) => {
   const thisTypes = types.filter(type => type.name.value === name);
-  if(thisTypes && thisTypes[0]){
+  if (thisTypes && thisTypes[0]) {
     return thisTypes[0].fields;
   }
   return [];
-}
+};
 
 module.exports = {
-  plugin: (schema, documents, config) => {
-    const genType = config.genType;
-    const modelName = config.modelName;
+  // eslint-disable-next-line consistent-return
+  plugin: (schema, _documents, config) => {
+    const { genType } = config;
+    const { modelName } = config;
     const printedSchema = printSchema(schema);
     const astNode = parse(printedSchema);
     const visitor = {
       FieldDefinition: node => {
         const type = parseType(node.type);
-        type.name=node.name.value;
+        type.name = node.name.value;
         return type;
       },
-      ObjectTypeDefinition: node => {
-        return node;
-      },
+      ObjectTypeDefinition: node => node,
     };
     const result = visit(astNode, { leave: visitor });
     const fields = extractFields(modelName, result.definitions);
     switch (genType) {
       case 'modelSource':
         return getModelSource(modelName, fields.slice());
-      case 'modelUpsert': 
-        return getModelUpsert(modelName, fields.slice() );
+      case 'modelUpsert':
+        return getModelUpsert(modelName, fields.slice());
       case 'modelView':
-          return getModelView(modelName, fields.slice() );
+        return getModelView(modelName, fields.slice());
       case 'modelDelete':
-          return getModelDelete(modelName, fields.slice() );
+        return getModelDelete(modelName, fields.slice());
       case 'modelsIndex':
-          return getModelsIndex(modelName);
+        return getModelsIndex();
       case 'indexModel':
-          return getIndexModel(modelName);
+        return getIndexModel(modelName);
       case 'modelModel':
-          return getModelModel(modelName);
+        return getModelModel(modelName);
       case 'modelsComponent':
-          return getModelsSource(modelName);
+        return getModelsSource();
       case 'modelsSource':
-          return getPageModels(modelName,config.nodes);
-    
+        return getPageModels(modelName, config.nodes);
+
       default:
-                console.log("ZONED OUT - go fix something",genType );
-          }
+        console.log('ZONED OUT - go fix something', genType);
+    }
   },
 };
